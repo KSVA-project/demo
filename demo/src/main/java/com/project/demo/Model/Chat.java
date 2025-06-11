542f24ff8650f10820e2f561f586d4eaba4ddc4f
sysshk <sysshk@naver.com>
2025-06-11 20:07:00 +0900

    main chatting del, update, fastAPI connection

--- demo/src/main/java/com/project/demo/Model/Chat.java
+++ demo/src/main/java/com/project/demo/Model/Chat.java
@@ -15,23 +15,23 @@ public class Chat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CHAT_IDX")
-    private Integer chatIdx;
+    private Integer chatIdx; // 채팅 식별자
 
     // fetch = FetchType.LAZY => 지연 로딩 방식,
     // chat 객체를 불러와도 실제
     @ManyToOne(fetch = FetchType.LAZY) // ManyToOne 관계
     @JoinColumn(name = "CROOM_IDX", nullable = false)
-    private ChatRoom chatRoom;
+    private ChatRoom chatRoom; // 채팅방 식별자 외래키(ChatRoom)
 
     @Column(name = "CHATTER", nullable = false, length = 20)
-    private String chatter;
+    private String chatter; // 발화자
 
     @Column(name = "CHAT", columnDefinition = "TEXT")
-    private String chat;
+    private String chat; // 채팅 내용
 
     @Column(name = "RATINGS", length = 1)
-    private String ratings;
+    private String ratings; // 채팅 평가 점수
 
     @Column(name = "CREATED_AT", nullable = false, updatable = false)
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt = LocalDateTime.now(); // 채팅보낸 시간
 }
